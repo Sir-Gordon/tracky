3ee53b9c475a01aab95e528e8e31f46a6e152fd1
slykel <39887933+slykel@users.noreply.github.com>
2019-03-06 00:26:31 -0500

    a

--- Portfolios.js
+++ Portfolios.js
@@ -6,6 +6,11 @@ var mysql = require('mysql');
 var app = express();  
 app.use(express.static(".")); 
 
+app.listen(8080, function(){    
+    console.log('Server Started...')
+});
+
+
 var apikey = '4914ER72DROICJLG'
 var URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=' + apikey
 
@@ -16,22 +21,27 @@ request({url: URL}, function(err,res,body){
 
 });
 
-
+var itemtosend = [];
 app.get("/searchlist", function (req, resp){
     var searchterm = req.query.searchterm;
-    var apikey = '4914ER72DROICJLG'
     console.log(searchterm);
+    
     URL = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+ searchterm + '&apikey=' + apikey;
-    console.log(URL);
 
     request({url: URL}, function(err,res,body){
-        console.log(body);
         
-    })
-    resp.jsonp(body);
+        itemtosend.push(body);
+        // console.log(body);
+    });
+
+    console.log(itemtosend);
+    resp.jsonp(itemtosend);
 });
 
-app.listen(8080, function(){    
-    console.log('Server Started...')
-});
+
+
+
+
+
+
 
