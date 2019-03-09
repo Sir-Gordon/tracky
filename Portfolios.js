f015fcc5c468ac54ca64e047e7ecac4052e1cc93
slykel <39887933+slykel@users.noreply.github.com>
2019-03-09 02:52:13 -0500

    almost

--- Portfolios.js
+++ Portfolios.js
@@ -11,32 +11,46 @@ app.listen(8080, function(){
 });
 
 
-var apikey = '4914ER72DROICJLG'
-var URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=' + apikey
 
 
-request({url: URL}, function(err,res,body){
-    
-    // console.log(body);
+function getsearchterm(SearchURL,cb,data) {
 
-});
-
-var msg = []
-app.get("/searchlist", function (req, resp){
-    var searchterm = req.query.searchterm;
-    console.log(searchterm);
-    
-    URL = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+ searchterm + '&apikey=' + apikey;
-
-    request({url: URL}, function(err,res,body){
-        let json = JSON.parse(body)
-        msg.push(json);
+    request({url: SearchURL, method:"get"}, function(err,res,body){
+        if(cb) {
+            cb(err, JSON.parse(body))
+        }
+        
     });
-    var msg1 = JSON.parse(msg[0]);
-    console.log(msg1);
-    resp.msg1;
+}
+
+var counter = 0;
+var apinum = 0;
+app.get("/searchlist", function (req, resp){
+    apikey = ['4914ER72DROICJLG','Q4CFZAGYPRRD9GRI','XZTDJ07EAQK567OH','FGCCJ0BEM65D8K34','716QGRRMABZUDE87'];
+    counter += 1;
     
+    if (counter == 6){
+        counter = 0;
+        apinum += 1;
+    if (apinum == 5){
+        apinum = 0;
+    }
+    }
+    
+    console.log(apinum);
+    console.log(apikey[apinum]);
+    var searchterm = req.query.searchterm;
+    var SearchURL = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+ searchterm + '&apikey=' + apikey[apinum];
+    console.log(SearchURL)
+    getsearchterm(SearchURL, function(err,data){
+        console.log(counter);
+        if (data.Note == 'Thank you for using Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day. Please visit https://www.alphavantage.co/premium/ if you would like to target a higher API call frequency. Thank you!'){
+            console.log("Thanks");
+            }
+        resp.jsonp(data);
+    });
 });
+
 
 
 
