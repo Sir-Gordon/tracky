d57615d652a659b67ef008a316a8fdb2c515b518
Corinna Hoang <ch942@drexel.edu>
2019-03-11 19:37:12 -0400

    a

--- Portfolios.js
+++ Portfolios.js
@@ -10,19 +10,24 @@ app.listen(8080, function(){
     console.log('Server Started...')
 });
 
+<<<<<<< HEAD
 function getsearchterm(SearchURL,cb,data) {
+=======
 
-    request({url: SearchURL, method:"get"}, function(err,res,body){
-        if(cb) {
-            cb(err, JSON.parse(body))
-        }
-        
-    });
-}
+var apikey = '4914ER72DROICJLG'
+var URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=' + apikey
 
-var counter = 0;
-var apinum = 0;
+
+request({url: URL}, function(err,res,body){
+    
+    // console.log(body);
+>>>>>>> parent of f015fcc... almost
+
+});
+
+var msg = []
 app.get("/searchlist", function (req, resp){
+<<<<<<< HEAD
     apikey = '4914ER72DROICJLG','Q4CFZAGYPRRD9GRI','XZTDJ07EAQK567OH''FGCCJ0BEM65D8K34','716QGRRMABZUDE87','WAI4B5EWWE6FLWQX','YCLTK7E9NR4THMFF','UL7XW4GHCHZI2LTZ','HH3TFTFAY00CP354','CQZGBZ02N3KH8XD3';
     counter += 1;
     
@@ -36,18 +41,22 @@ app.get("/searchlist", function (req, resp){
     
     console.log(apinum);
     console.log(apikey[apinum]);
+=======
+>>>>>>> parent of f015fcc... almost
     var searchterm = req.query.searchterm;
-    var SearchURL = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+ searchterm + '&apikey=' + apikey[apinum];
-    console.log(SearchURL)
-    getsearchterm(SearchURL, function(err,data){
-        console.log(counter);
-        if (data.Note == 'Thank you for using Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day. Please visit https://www.alphavantage.co/premium/ if you would like to target a higher API call frequency. Thank you!'){
-            console.log("Thanks");
-            }
-        resp.jsonp(data);
-    });
-});
+    console.log(searchterm);
+    
+    URL = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+ searchterm + '&apikey=' + apikey;
 
+    request({url: URL}, function(err,res,body){
+        let json = JSON.parse(body)
+        msg.push(json);
+    });
+    var msg1 = JSON.parse(msg[0]);
+    console.log(msg1);
+    resp.msg1;
+    
+});
 
 
 
