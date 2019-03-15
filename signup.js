abacea96f46a6f8adda1612ad9552a20a20b5da1
Kim Tran <ngankimtran217@yahoo.com>
2019-03-15 02:42:15 -0400

    .

--- signup.js
+++ signup.js
@@ -18,39 +18,21 @@ con.connect(function (err) {
     }
 });
 
-app.post('/Signup', function(req, res){
-    var name = req.query.name;
-    console.log(name);
-    var username = req.query.username;
-    var password = req.query.password;
-    var mysql  = "INSERT INTO reg(name,username,password) VALUES('" + name + "','" + username + "','" + password +"')";
-    con.query(mysql, function(err,rows,fields){
-        if (err){
-            console.log('Error during query processing');
-        }
-        else{
-            res.send("You are in our system!")
-        }
-    }); 
+// adds new students into the student table in the database
+app.post('/Signup', function (req, res) {
+    var name = req.query.n; //req.query contains the URL query parameters, which is the user's input (after the ? in the URL)
+	var username = req.query.username;
+	var pw = req.query.pw;
+    var mysql = "INSERT INTO reg (name, username, password) VALUES ('" + name + "','" + username + "','" + pw + "')";
+    con.query(mysql, function(err,rows,fields) {
+	 	if (err){
+	 		console.log('Error during query processing');
+	 	}
+	 	else {
+	 		res.send("You've been added!");
+	 	}
+	 });
 });
-
-//// adds new students into the student table in the database
-//app.post('/add', function (req, res) {
-//    var id = req.query.id; //req.query contains the URL query parameters, which is the user's input (after the ? in the URL)
-//	var first = req.query.first;
-//	var last = req.query.last;
-//	var dob = req.query.dob;
-//	var major = req.query.major;
-//    var mysql = "INSERT INTO student (studentid, first_name, last_name, dob, major) VALUES ('" + id + "','" + first + "','" + last + "','" + dob + "','" + major + "')";
-//    con.query(mysql, function(err,rows,fields) {
-//	 	if (err){
-//	 		console.log('Error during query processing');
-//	 	}
-//	 	else {
-//	 		res.send("You've been added!");
-//	 	}
-//	 });
-//});
 
 
 app.listen(8080, function () { //listens to the port 8080
