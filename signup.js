1abea95dca0face087c8db4124141dd8eec0d9bb
Kim Tran <ngankimtran217@yahoo.com>
2019-03-17 15:04:14 -0400

    oh my lord......bitch

--- signup.js
+++ signup.js
@@ -3,8 +3,8 @@ var app = express();
 app.use(express.static("."));
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
-var urlencodedParser = (bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json()); // Body parser use JSON data
 
 // Connect to MYSQL
 var con = mysql.createConnection({ //sets up mysql database
@@ -22,24 +22,23 @@ con.connect(function (err) {
         console.log('Database successfully connected');
     }
 });
-// urlencodedParser will pass the user's data, which can then be accessed by the req
-app.post('/Signup', urlencodedParser, function (req, res) {
-    console.log(req.body);
-//    var name = req.body.name; 
-//	var username = req.body.username;
-//	var pw = req.body.pw;
-//    console.log(JSON.stringify(name));
-//    var mysql = "INSERT INTO users (name, username, password) VALUES ('" + name + "','" + username + "','" + pw + "')";
-//    con.query(mysql, function(err,rows,fields) {
-//	 	if (err){
-//	 		console.log('Error during query processing');
-//	 	}
-//	 	else {
-//	 		res.send("You've been added!");
-//	 	}
-//	 });
-});
 
+// urlencodedParser will pass the user's data, which can then be accessed by the req
+app.post('/Signup', function (req, res) {
+    console.log(JSON.stringify(req.body, 2));
+    var name = req.body.name;
+    var username = req.body.username;
+    var pw = req.body.pw;
+    var mysql = "INSERT INTO users (name, username, password) VALUES ('" + name + "','" + username + "','" + pw + "')";
+    con.query(mysql, function(err,rows,fields) {
+        if (err){
+            console.log('Error during query processing' + err);
+        }
+        else {
+            res.send("Your registration has been complete!");
+        }
+    });
+})
 
 app.listen(8080, function () { //listens to the port 8080
     console.log("SERVER IS STARTING");
